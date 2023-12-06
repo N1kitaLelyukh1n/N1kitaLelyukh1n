85015db2af19e7ea477fe15190ca152cbf923330
N1kitaLelyukh1n <nikitalelyukhin@yandex.ru>
2023-12-06 18:21:54 +0300

    initial commit

---
+++ src/main/java/ru/mpei/MyBD/MyBdApplication.java
@@ -0,0 +1,13 @@
+package ru.mpei.MyBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mpei/MyBD/MyBdApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mpei.MyBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
